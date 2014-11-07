91a8645898a571a946210bc44b6ec6b9ca51e531
psychomantys <psycho.mantys@gmail.com>
2014-11-07 18:09:32 -0300

    Corrigindo pequeno erro indicando local errado de arquivo.
    Esse script esta meio seboso, mas esta valendo a inteção, eu acho.

--- tools/gen_signed_align_release.sh
+++ tools/gen_signed_align_release.sh
@@ -8,7 +8,7 @@ unsigned_unalign_release_apk="${1}"
 
 $keytool -genkey -v -keystore ~/.android/release.keystore -alias androidreleasekey -storepass android -keypass android -keyalg RSA -validity 14000
 
-$jarsigner -sigalg MD5withRSA -digestalg SHA1 -keystore release.keystore -signedjar release-signed-unalign.apk  "${unsigned_unalign_release_apk}" androidreleasekey
+$jarsigner -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/release.keystore -signedjar release-signed-unalign.apk  "${unsigned_unalign_release_apk}" androidreleasekey
 
 $zipalign -v -f 4  release-signed-unalign.apk "${2}"
 
