9e2c1d3b0a97fd48992989f56859a77c0a172021
psychomantys <psycho.mantys@gmail.com>
2014-11-09 23:00:01 -0300

    Algumas coisas corrigidas, usei uma versão antiga, mas agora esta certo.

--- tools/gen_signed_align_release.sh
+++ tools/gen_signed_align_release.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-jarsigner=/usr/java/latest/bin/jarsigner
-keytool=/usr/java/latest/bin/keytool
+jarsigner="${JAVA_HOME}/bin/jarsigner"
+keytool="${JAVA_HOME}/bin/keytool"
 zipalign=~/android-sdk-linux/build-tools/20.0.0/zipalign
 
 unsigned_unalign_release_apk="${1}"
 
 $keytool -genkey -v -keystore ~/.android/release.keystore -alias androidreleasekey -storepass android -keypass android -keyalg RSA -validity 14000
 
-$jarsigner -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/release.keystore -signedjar release-signed-unalign.apk  "${unsigned_unalign_release_apk}" androidreleasekey
+$jarsigner -keypass 'android' -storepass 'android' -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/release.keystore -signedjar release-signed-unalign.apk  "${unsigned_unalign_release_apk}" androidreleasekey
 
 $zipalign -v -f 4  release-signed-unalign.apk "${2}"
-
