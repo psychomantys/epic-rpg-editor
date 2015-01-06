11837a637fa20250e6a23fa49746338dbdbec0a4
Vecksagos <bsb@ic.ufal.br>
2015-01-06 15:42:35 -0300

    Criação da diretiva de menu, e colocando as coisas nos modulos certos

--- app_modules/main/main.js
+++ app_modules/main/main.js
@@ -1,4 +1,36 @@
-angular.module("bsb.main", [])
+angular.module("bsb.main", ["ui.router"])
+    .config(["$stateProvider", "$urlRouterProvider",
+             function($stateProvider, $urlRouterProvider) {
+                 $urlRouterProvider.otherwise("/3det/informacoes");
+
+                 $stateProvider
+                     .state("3det",
+                            {
+                                abstract: true,
+                                url: "/3det",
+                                templateUrl: "app_modules/main/main.html"
+                            })
+                     .state("3det.informacoes",
+                            {
+                                url: "/informacoes",
+                                templateUrl: "app_modules/informacoes/informacoes.html"
+                            })
+                     .state("3det.vantagens",
+                            {
+                                url: "/vantagens",
+                                templateUrl: "app_modules/vantagens/vantagens.html"
+                            })
+                     .state("3det.desvantagens",
+                            {
+                                url: "/desvantagens",
+                                templateUrl: "app_modules/desvantagens/desvantagens.html"
+                            })
+                     .state("3det.pericias",
+                            {
+                                url: "/pericias",
+                                templateUrl: "app_modules/pericias/pericias.html"
+                            });
+             }])
     .controller("MainCtrl", ["$scope", "$state",
                               function($scope, $state) {
                                   $scope.tabs = [
